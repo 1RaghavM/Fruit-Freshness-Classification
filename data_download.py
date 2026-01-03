66894351aa5792d656e56ee7b17794d7fe190592
1RaghavM <lebrongoated2323@gmail.com>
2026-01-03 13:53:15 -0500

    Initial Commit; Setup of modules dir, and begin data preprocessing

---
+++ data_download.py
@@ -0,0 +1,5 @@
+import kagglehub
+
+path = kagglehub.dataset_download("abdulrafeyyashir/fresh-vs-rotten-fruit-images")
+
+print("Path to dataset files:", path)
---
+++ modules/data_setup.py
@@ -0,0 +1,21 @@
+import os
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
+
+def create_dataloaders(train_dir, test_dir, transform, batch_size, num_workers):
+    train_data = datasets.ImageFolder(train_dir, transforms=transform)
+    test_data = datasets.ImageFolder(test_dir, transforms=transform)
+
+    class_names = train_data.classes
+
+    train_dataloader = DataLoader(train_data, batch_size=batch_size, shuffle=True, num_workers=num_workers, pin_memory=True)
+    test_dataloader = DataLoader(test_data, batch_size=batch_size, shuffle=False, num_workers=num_workers, pin_memory=True)
+
+    return train_dataloader, test_dataloader
+
+
+# train_data = datasets.ImageFolder('/Users/raghavmehta/Downloads/Fruit-Freshness-Classification/modules/data/train')
+# test_data = datasets.ImageFolder('/Users/raghavmehta/Downloads/Fruit-Freshness-Classification/modules/data/test')
+
+# class_names = train_data.classes
+# print(class_names)
